ca63b0b18ec58479a616f963686075756ac5e389
liza <liz.belaya2011@yandex.ru>
2019-10-30 17:56:09 +0300

    First commit

---
+++ qr/models.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Qr(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    campaign = models.CharField(max_length=200)
+    sourse =  models.CharField(max_length=200)
+    product =  models.CharField(max_length=200)
+    #title = models.CharField(max_length=200)
+    #text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    number_of_transitions = models.IntegerField(default=0)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+#    def __str__(self):
+#        return self.title
